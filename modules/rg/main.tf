55c97b59968d25130cd58a29d71e8f5ddb2f18b1
Helen San <san.helen1@gmail.com>
2022-10-21 00:54:24 +0000

    Updated main.tf

--- modules/rg/main.tf
+++ modules/rg/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "default" {
-    name = "rg-${var.rg_short_location}-${var.env}-${var.app_name}"
+    name = "rg-${var.rg_location_short}-${var.env}-${var.app_name}"
     location = var.rg_location
 }
